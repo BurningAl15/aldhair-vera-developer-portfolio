9f951012e7401282e53c759bea94a0c53d8a7af6
Aldhair Vera <aldhairvera15@gmail.com>
2023-06-07 13:42:13 -0500

    Improvements in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer class="rounded-lg shadow mt-4 mr-4 ml-4">
       <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Made by Aldhair Vera All Rights Reserved.
         </span>
